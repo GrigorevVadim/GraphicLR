62eb7e7ed463444b18cb6530ccbf0cab263bc188
Григорьев Вадим <grigorev.v@skbkontur.ru>
2020-05-10 17:40:01 +0500

    Ещё раз переименовал

--- LR1/Properties/AssemblyInfo.cs
+++ LR1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GeometricModelingLR1")]
+[assembly: AssemblyTitle("LR1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GeometricModelingLR1")]
+[assembly: AssemblyProduct("LR1")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
